f594c4df3e252c2cacfd59fc6621cff9b32f0076
Richard Kotze <rkotze@findmypast.com>
2018-12-30 16:05:51 +0000

    Check git extension is ready before using API
    On first load of vscode and going straight away to SCM view the extension api or there repository may not be loaded causing an error.

--- src/update-scm-input.js
+++ src/update-scm-input.js
@@ -4,14 +4,17 @@ const path = require("path");
 const vscode = require("vscode");
 
 exports.updateSCMInput = function updateSCMInput(coAuthors) {
-  const gitExt = vscode.extensions.getExtension("vscode.git").exports;
-  if (gitExt) {
-    const firstRepository = gitExt.getAPI(1).repositories[0];
-    const inputBox = firstRepository.inputBox;
-    inputBox.value = replaceCoAuthors(
-      inputBox.value,
-      formatCoAuthors(coAuthors)
-    );
+  const gitExt = vscode.extensions.getExtension("vscode.git");
+  if (gitExt && gitExt.isActive) {
+    const api = gitExt.exports.getAPI(1);
+    if (api.repositories.length) {
+      const firstRepository = api.repositories[0];
+      const inputBox = firstRepository.inputBox;
+      inputBox.value = replaceCoAuthors(
+        inputBox.value,
+        formatCoAuthors(coAuthors)
+      );
+    }
   }
 };
 
