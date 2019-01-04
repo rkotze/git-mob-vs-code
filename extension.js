c7d4fa921493a117148b63b8d2cdf81c16ddd33e
Richard Kotze <rkotze@findmypast.com>
2019-01-04 13:52:03 +0000

    Open git-coauthors file from command palette
    Contributes to: #6

--- extension.js
+++ extension.js
@@ -1,7 +1,9 @@
 const { setupGitMob } = require("./src/git-mob-list");
+const { openGitCoAuthor } = require("./src/open-git-coauthors");
 
 function activate(context) {
   setupGitMob(context);
+  openGitCoAuthor(context);
 }
 exports.activate = activate;
 
---
+++ src/open-git-coauthors.js
@@ -0,0 +1,29 @@
+const os = require("os");
+const path = require("path");
+const vscode = require("vscode");
+
+function openGitCoAuthor(context) {
+  let openFile = vscode.commands.registerCommand(
+    "gitmob.openGitCoauthor",
+    async function() {
+      const { openTextDocument } = vscode.workspace;
+      const { showTextDocument } = vscode.window;
+
+      try {
+        const pathToCoauthors = vscode.Uri.file(
+          path
+            .join(os.homedir(), ".git-coauthors")
+            .replace(/^[a-z]:[\\]|[\/]/gi, "")
+        );
+        const doc = await openTextDocument(pathToCoauthors);
+        showTextDocument(doc);
+      } catch (err) {
+        vscode.window.showErrorMessage("GitMob error: " + err);
+      }
+    }
+  );
+
+  context.subscriptions.push(openFile);
+}
+
+exports.openGitCoAuthor = openGitCoAuthor;
