2e986fd23aea72586b675264e4a2e20433fb5009
Richard Kotze <rkotze@findmypast.com>
2019-01-10 17:36:01 +0000

    Add in vscode mock for jest and pass unit test
    Since vs code module is only available in VS code environment it needs to be mocked out.

--- src/mob-author.spec.js
+++ src/mob-author.spec.js
@@ -1,21 +1,31 @@
-const { mob } = require("./commands");
+// const commandsMock = {
+//   mob: {
+//     listAll: jest.fn()
+//   }
+// };
 
-jest.mock("./commands", () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      mob: {
-        listAll: function() {
-          return "rk richard kotze <rkotze@email.com>";
-        }
-      }
-    };
-  });
-});
-
+const commands = require("./commands");
 const { MobAuthors } = require("./mob-authors");
 
-test("Creates a co author", () => {
-  let a = new MobAuthors();
+jest.mock("./commands");
 
-  expect(a.listAll).toHaveLength(1);
+test("Creates a co author", () => {
+  commands.mob.listAll.mockReturnValue(
+    `rk richard kotze rkotze@email.com\nts Tony Stark tony@stark.com`
+  );
+  // const listAll = jest.fn().mockReturnValue(
+  //   `rk richard kotze <rkotze@email.com>
+  //   ts Tony Stark <tony@stark.com>`
+  // );
+  // commands.mockImplementation(() => ({
+  //   mob: {
+  //     listAll
+  //   }
+  // }));
+
+  let mobAuthors = new MobAuthors();
+  let coAuthorList = mobAuthors.listAll;
+
+  expect(commands.mob.listAll).toHaveBeenCalled();
+  expect(coAuthorList).toHaveLength(2);
 });
