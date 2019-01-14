d0796a8d14c4682625f1210b7dcd8a8231677020
Richard Kotze <rkotze@findmypast.com>
2019-01-14 18:24:56 +0000

    Remove commented examples for mocking in Jest

--- src/mob-author.spec.js
+++ src/mob-author.spec.js
@@ -1,9 +1,3 @@
-// const commandsMock = {
-//   mob: {
-//     listAll: jest.fn()
-//   }
-// };
-
 const commands = require("./commands");
 const { MobAuthors } = require("./mob-authors");
 
@@ -13,15 +7,6 @@ test("Creates a co author", () => {
   commands.mob.listAll.mockReturnValue(
     `rk richard kotze rkotze@email.com\nts Tony Stark tony@stark.com`
   );
-  // const listAll = jest.fn().mockReturnValue(
-  //   `rk richard kotze <rkotze@email.com>
-  //   ts Tony Stark <tony@stark.com>`
-  // );
-  // commands.mockImplementation(() => ({
-  //   mob: {
-  //     listAll
-  //   }
-  // }));
 
   let mobAuthors = new MobAuthors();
   let coAuthorList = mobAuthors.listAll;
