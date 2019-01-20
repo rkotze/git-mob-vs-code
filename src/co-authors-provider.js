75bd4d69e1071e9bf622a1d8c11eb395d8e3265d
Richard Kotze <richkotze@outlook.com>
2019-01-20 21:43:59 +0000

    :recycle: move the co author provider into its own file

---
+++ src/co-authors-provider.js
@@ -0,0 +1,65 @@
+const vscode = require("vscode");
+const { MobAuthors } = require("./mob-authors");
+const { TreeNode } = require("./tree-node");
+
+class CoAuthorProvider {
+  constructor(context) {
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    this.mobAuthors = new MobAuthors();
+    this._notLoaded = true;
+    this.context = context;
+  }
+
+  getChildren(element = {}) {
+    const allAuthors = this.mobAuthors.listAll;
+    if (element.key === "Selected") {
+      return this.mobAuthors.listCurrent;
+    }
+
+    if (element.key === "Unselected") {
+      const setAllAuthor = new Set(allAuthors);
+      for (let author of setAllAuthor) {
+        if (author.selected) setAllAuthor.delete(author);
+      }
+      return Array.from(setAllAuthor);
+    }
+
+    return [
+      this.mobAuthors.author,
+      new TreeNode("Selected", "selected"),
+      new TreeNode("Unselected")
+    ];
+  }
+
+  getTreeItem(element) {
+    if (
+      element.email === this.mobAuthors.lastCoAuthor.email &&
+      this._notLoaded
+    ) {
+      this.loaded();
+      this._notLoaded = false;
+    }
+
+    if (
+      element.email === this.mobAuthors.lastCoAuthor.email &&
+      !this._notLoaded
+    ) {
+      this.onUpdated();
+    }
+    return element.getTreeItem({
+      iconPath: this.context.asAbsolutePath("resources/icons/user.svg")
+    });
+  }
+
+  toggleCoAuthor(author, selected) {
+    this.mobAuthors.setCurrent(author, selected);
+    this.reloadData();
+  }
+
+  reloadData() {
+    this._onDidChangeTreeData.fire();
+  }
+}
+
+exports.CoAuthorProvider = CoAuthorProvider;
--- src/setup-git-mob.js
+++ src/setup-git-mob.js
@@ -1,7 +1,6 @@
 const vscode = require("vscode");
+const { CoAuthorProvider } = require("./co-authors-provider");
 const { updateSCMInput } = require("./update-scm-input");
-const { MobAuthors } = require("./mob-authors");
-const { TreeNode } = require("./tree-node");
 const { reloadOnSave } = require("./reload-on-save");
 const { reloadCommand } = require("./commands/reload");
 const { openGitCoAuthor } = require("./commands/open-git-coauthors");
@@ -52,63 +51,3 @@ function setupGitMob(context) {
 }
 
 exports.setupGitMob = setupGitMob;
-
-class CoAuthorProvider {
-  constructor(context) {
-    this._onDidChangeTreeData = new vscode.EventEmitter();
-    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-    this.mobAuthors = new MobAuthors();
-    this._notLoaded = true;
-    this.context = context;
-  }
-
-  getChildren(element = {}) {
-    const allAuthors = this.mobAuthors.listAll;
-    if (element.key === "Selected") {
-      return this.mobAuthors.listCurrent;
-    }
-
-    if (element.key === "Unselected") {
-      const setAllAuthor = new Set(allAuthors);
-      for (let author of setAllAuthor) {
-        if (author.selected) setAllAuthor.delete(author);
-      }
-      return Array.from(setAllAuthor);
-    }
-
-    return [
-      this.mobAuthors.author,
-      new TreeNode("Selected", "selected"),
-      new TreeNode("Unselected")
-    ];
-  }
-
-  getTreeItem(element) {
-    if (
-      element.email === this.mobAuthors.lastCoAuthor.email &&
-      this._notLoaded
-    ) {
-      this.loaded();
-      this._notLoaded = false;
-    }
-
-    if (
-      element.email === this.mobAuthors.lastCoAuthor.email &&
-      !this._notLoaded
-    ) {
-      this.onUpdated();
-    }
-    return element.getTreeItem({
-      iconPath: this.context.asAbsolutePath("resources/icons/user.svg")
-    });
-  }
-
-  toggleCoAuthor(author, selected) {
-    this.mobAuthors.setCurrent(author, selected);
-    this.reloadData();
-  }
-
-  reloadData() {
-    this._onDidChangeTreeData.fire();
-  }
-}
