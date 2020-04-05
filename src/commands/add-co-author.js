a2e2fbeb885c8f3b1007a48d3f431192df9ba7b0
Richard Kotze <richkotze@outlook.com>
2020-04-05 23:59:44 +0100

    Manual input of new co-author
    - Enable users to input a co-author manually using vs code inputs
    - Rename commands as they are too similar but do different things. This will help reduce confusion.
    
    Related issue: #4

--- src/commands/add-co-author.js
+++ src/commands/add-co-author.js
@@ -5,13 +5,35 @@ function addRepoAuthorToCoauthors({ coAuthorProvider }) {
   const { context } = coAuthorProvider;
   let disposableAddRepoAuthor = vscode.commands.registerCommand(
     "gitmob.addRepoAuthorToCoAuthors",
-    function(author) {
-      addRepoAuthor(author);
+    async function (author) {
+      if (author) {
+        addRepoAuthor(author);
+      } else {
+        addRepoAuthor(await inputAuthorData());
+      }
       vscode.commands.executeCommand("gitmob.reload");
     }
   );
 
   context.subscriptions.push(disposableAddRepoAuthor);
+}
+
+async function inputAuthorData() {
+  const name = await vscode.window.showInputBox({
+    prompt: "Author name",
+  });
+  const email = await vscode.window.showInputBox({
+    prompt: "Author email",
+  });
+  const commandKey = await vscode.window.showInputBox({
+    prompt: "Author initials",
+  });
+
+  return {
+    name,
+    email,
+    commandKey,
+  };
 }
 
 exports.addRepoAuthorToCoauthors = addRepoAuthorToCoauthors;
