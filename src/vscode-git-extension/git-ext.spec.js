1356c3de05a731fa781b5334f701ffe79e26ed40
Richard Kotze <richkotze@outlook.com>
2020-07-04 18:07:13 +0100

    :white_check_mark: no selected repos will return fspath of first one
    Default to selecting the first repo for a fspath when no repos are selected in vscode git extension.
    
    #39

--- src/vscode-git-extension/git-ext.spec.js
+++ src/vscode-git-extension/git-ext.spec.js
@@ -3,7 +3,7 @@ const { vsCodeGit } = require("./vs-code-git");
 
 jest.mock("./vs-code-git");
 
-test("returns a selected repository file path", () => {
+test("returns selected repository file path", () => {
   vsCodeGitRepo([
     {
       ui: {
@@ -16,6 +16,29 @@ test("returns a selected repository file path", () => {
   ]);
   const gitExt = new GitExt();
   expect(gitExt.rootPath).toEqual("project/fspath");
+});
+
+test("returns a file path when there are repositories but none are selected", () => {
+  vsCodeGitRepo([
+    {
+      ui: {
+        selected: false,
+      },
+      rootUri: {
+        fsPath: "first/project/fspath",
+      },
+    },
+    {
+      ui: {
+        selected: false,
+      },
+      rootUri: {
+        fsPath: "should/not/find/me",
+      },
+    },
+  ]);
+  const gitExt = new GitExt();
+  expect(gitExt.rootPath).toEqual("first/project/fspath");
 });
 
 test("returns null when no repositories", () => {
