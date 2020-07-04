e0dd933f007520d77960ae409cb75331179448d1
Richard Kotze <richkotze@outlook.com>
2020-07-04 18:01:36 +0100

    :white_check_mark: no path if no repositories
    #39

--- src/vscode-git-extension/git-ext.spec.js
+++ src/vscode-git-extension/git-ext.spec.js
@@ -3,24 +3,31 @@ const { vsCodeGit } = require("./vs-code-git");
 
 jest.mock("./vs-code-git");
 
-beforeAll(() => {
-  vsCodeGit.mockImplementation(() => {
-    return {
-      repositories: [
-        {
-          ui: {
-            selected: true,
-          },
-          rootUri: {
-            fsPath: "project/fspath",
-          },
-        },
-      ],
-    };
-  });
-});
-
 test("returns a selected repository file path", () => {
+  vsCodeGitRepo([
+    {
+      ui: {
+        selected: true,
+      },
+      rootUri: {
+        fsPath: "project/fspath",
+      },
+    },
+  ]);
   const gitExt = new GitExt();
   expect(gitExt.rootPath).toEqual("project/fspath");
 });
+
+test("returns null when no repositories", () => {
+  vsCodeGitRepo([]);
+  const gitExt = new GitExt();
+  expect(gitExt.rootPath).toEqual(null);
+});
+
+function vsCodeGitRepo(repositories) {
+  vsCodeGit.mockImplementation(() => {
+    return {
+      repositories,
+    };
+  });
+}
