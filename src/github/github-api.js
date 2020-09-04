6b9a5165ad048a8e7c32581d274141717bab084a
Richard Kotze <richkotze@outlook.com>
2020-09-04 18:41:15 +0100

    All user to set GitHub PAT
    A key is needed to search GitHub and to get the users email address. #15

--- src/github/github-api.js
+++ src/github/github-api.js
@@ -1,14 +1,20 @@
+const vscode = require("vscode");
 const { fetch } = require("./fetch");
 
 const GITHUB_API = "https://api.github.com/";
 
 async function get(path) {
+  const gitHubSettings = vscode.workspace.getConfiguration("gitMob.gitHub");
+  const pat = gitHubSettings.get("personalAccessToken");
+  if (!pat) {
+    throw new Error("No GitHub personal access token found");
+  }
   try {
     const result = await fetch(GITHUB_API + path, {
       method: "GET",
       headers: {
         Accept: "application/vnd.github.v3+json",
-        Authorization: "token <tokenhere>",
+        Authorization: `token ${pat}`,
       },
     });
 
