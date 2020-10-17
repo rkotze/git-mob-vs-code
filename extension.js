7c18a4ce7f41728d3d65d0761b78ac4c0154c450
Richard Kotze <richkotze@outlook.com>
2020-10-17 14:14:39 +0100

    Install options in git mob view container
    Provide install buttons for git mob cli to choose between global or local.
    
    #44

--- extension.js
+++ extension.js
@@ -1,14 +1,14 @@
 const { mob } = require("./src/git/commands");
 const { setupGitMob } = require("./src/setup-git-mob");
 const { GitExt } = require("./src/vscode-git-extension/git-ext");
-const { installPrompt } = require("./src/install-prompt");
 const { waitForRepos } = require("./src/wait-for-repos");
+const { installCli } = require("./src/commands/install-cli");
 
 function activate(context) {
   const gitExt = new GitExt();
   waitForRepos(gitExt, () => {
     if (mob.gitMobLatest() === 1) {
-      installPrompt(() => {
+      installCli(context, () => {
         setupGitMob(context, gitExt);
       });
     } else {
