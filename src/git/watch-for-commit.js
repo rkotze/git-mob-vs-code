3d85cacd7bd0dd37d0abc9ce2743bfe60edd210d
Richard Kotze <richkotze@outlook.com>
2020-12-19 17:51:49 +0000

    :bug: Handle watch when no COMMIT_EDITMSG
    Wrap in try catch and provide an alert to log error. Should fail gracefully.
    
    #56

--- src/git/watch-for-commit.js
+++ src/git/watch-for-commit.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
 const path = require("path");
+const { logIssue } = require("../errors/log-issue");
 const { GitExt } = require("../vscode-git-extension/git-ext");
 
 exports.watchForCommit = function watchForCommit(cb) {
   const gitExt = new GitExt();
-  const gitCommit = path.join(gitExt.rootPath, ".git", "COMMIT_EDITMSG");
+  const gitCommit = path.join(gitExt.rootPath, ".git");
 
-  return fs.watch(gitCommit, function (evt, filename) {
-    if (filename) {
-      if (debounceFsWatch()) return;
-      cb(evt);
-    }
-  });
+  try {
+    return fs.watch(gitCommit, function (evt, filename) {
+      if (filename && filename === "COMMIT_EDITMSG") {
+        if (debounceFsWatch()) return;
+        cb(evt);
+      }
+    });
+  } catch (err) {
+    logIssue("Watch for commit failed!: " + err.message);
+  }
 };
 
 let fsWait = false;
