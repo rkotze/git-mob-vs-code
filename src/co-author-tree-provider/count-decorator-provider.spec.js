fea5d1d2e3543b7c324b2fb068a9acae32d66c7e
Richard Kotze <richkotze@outlook.com>
2020-12-24 16:55:09 +0000

    :white_check_mark: test counts for more & selected co-authors

--- src/co-author-tree-provider/count-decorator-provider.spec.js
+++ src/co-author-tree-provider/count-decorator-provider.spec.js
@@ -1,18 +1,30 @@
 const { CountDecorationProvider } = require("./count-decorator-provider");
 
 describe("Count co-authors", function () {
-  it("for selected co-authors", function () {
-    let countDecorator = new CountDecorationProvider({
-      mobAuthors: {
-        listAll: new Array(5),
-        listCurrent: new Array(2),
-        repoAuthorList: jest.fn().mockResolvedValue(new Array(30)),
-      },
-      onCoAuthorChange: jest.fn(),
-    });
+  let countDecorator = new CountDecorationProvider({
+    mobAuthors: {
+      listAll: new Array(5).fill({ selected: false }),
+      listCurrent: new Array(2),
+      repoAuthorList: jest.fn().mockResolvedValue(new Array(30)),
+    },
+    onCoAuthorChange: jest.fn(),
+  });
 
+  it("should be two selected", function () {
     expect(
       countDecorator.provideFileDecoration({ path: "/selected" })
     ).resolves.toEqual(expect.objectContaining({ badge: "2" }));
+  });
+
+  it("should be five unselected", function () {
+    expect(
+      countDecorator.provideFileDecoration({ path: "/unselected" })
+    ).resolves.toEqual(expect.objectContaining({ badge: "5" }));
+  });
+
+  it("should have 30 more authors", function () {
+    expect(
+      countDecorator.provideFileDecoration({ path: "/more-authors" })
+    ).resolves.toEqual(expect.objectContaining({ badge: "30" }));
   });
 });
