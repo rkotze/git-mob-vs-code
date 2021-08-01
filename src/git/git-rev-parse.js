f55a395fa16d9dbd67a9ddc8a599d12d93027d72
Richard Kotze <richkotze@outlook.com>
2021-08-01 17:34:35 +0100

    WIP: append co-authors to .gitmessage

---
+++ src/git/git-rev-parse.js
@@ -0,0 +1,41 @@
+const { gitVersion } = require("./commands");
+const { silentRun } = require("./silent-run");
+
+function gitPath(path) {
+  const version = silentRun("git --version").stdout.trim();
+  const [major, minor] = gitVersion(version);
+
+  if (major >= 2 && minor >= 13) {
+    return silentRun(`git rev-parse --git-path ${path}`).stdout.trim();
+  }
+
+  // Git pre-v2.13 does not give relative path to GIT_DIR for `rev-parse --git-path`.
+  // Prefix relative path with `--show-cdup`.
+  // Git release notes: https://github.com/git/git/blob/master/Documentation/RelNotes/2.13.0.txt#L71-L74
+  const relativePath = silentRun(
+    `git rev-parse --show-cdup && git rev-parse --git-path ${path}`
+  ).stdout.trim();
+  return relativePath.replace(/(\r\n|\r|\n)/, "");
+}
+
+/**
+ * Computes the path to the top-level directory of the git repository.
+ * @returns {string} Path to the top-level directory of the git repository.
+ */
+function topLevelDirectory() {
+  return silentRun("git rev-parse --show-toplevel").stdout.trim();
+}
+
+/**
+ * Checks if the current working directory is inside the working tree of a git repository.
+ * @returns {boolean} Is the cwd in a git repository?
+ */
+function insideWorkTree() {
+  return silentRun("git rev-parse --is-inside-work-tree").status === 0;
+}
+
+module.exports = {
+  topLevelDirectory,
+  gitPath,
+  insideWorkTree,
+};
