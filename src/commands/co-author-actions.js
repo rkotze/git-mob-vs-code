a753e724451066582b2be765e0acdb3ca7168d70
Richard Kotze <richkotze@outlook.com>
2022-02-13 17:44:08 +0000

    WIP: append co-authors via command palette
    #82

--- src/commands/co-author-actions.js
+++ src/commands/co-author-actions.js
@@ -1,10 +1,40 @@
 const vscode = require("vscode");
+// const { addNewCoAuthor } = require("../git/git-mob-api");
+const { GitExt } = require("../vscode-git-extension/git-ext");
+
+async function quickPickAuthors(repoAuthors) {
+  const gitExt = new GitExt();
+  const authorTextArray = repoAuthors.map((author) => ({
+    label: `${author.name} <${author.email}>`,
+    description: gitExt.selectedFolderName,
+    repoAuthor: { ...author, key: author.commandKey },
+    picked: author.selected,
+  }));
+  return vscode.window.showQuickPick(authorTextArray, { canPickMany: true });
+}
 
 function addCoAuthor({ coAuthorProvider }) {
+  const { mobAuthors } = coAuthorProvider;
   return vscode.commands.registerCommand(
     "gitmob.addCoAuthor",
-    function (author) {
-      return coAuthorProvider.toggleCoAuthor(author, true);
+    async function (author) {
+      if (author) {
+        return coAuthorProvider.toggleCoAuthor(author, true);
+      }
+      await mobAuthors.listCurrent();
+      const allSavedAuthors = await mobAuthors.listAll();
+      // const repoAuthors = await mobAuthors.repoAuthorList();
+      const authorItem = await quickPickAuthors([
+        ...allSavedAuthors,
+        // ...repoAuthors,
+      ]);
+      if (authorItem) {
+        await mobAuthors.setCurrent(
+          authorItem.map((author) => author.repoAuthor),
+          true
+        );
+        await vscode.commands.executeCommand("gitmob.reload");
+      }
     }
   );
 }
