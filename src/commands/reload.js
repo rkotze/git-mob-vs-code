cea3cc72ff47090f9c0128ec3fdf7b9de1c0f3c7
Richard Kotze <richkotze@outlook.com>
2023-12-09 16:00:09 +0000

    Fix: Need to await reloadData
    UI list in panel is not updated without awaiting.

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -1,8 +1,8 @@
 const vscode = require("vscode");
 
 function reloadCommand({ coAuthorProvider }) {
-  return vscode.commands.registerCommand("gitmob.reload", function () {
-    coAuthorProvider.coAuthorGroups.reloadData();
+  return vscode.commands.registerCommand("gitmob.reload", async function () {
+    await coAuthorProvider.coAuthorGroups.reloadData();
     coAuthorProvider.reloadData();
   });
 }
